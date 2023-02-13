8e79bd08ef11381828e6496ad8272fc8d676bf2e
ayansheikh6600 <ayansheikh6600@email.com>
2023-02-14 00:07:46 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,104 @@
+// Chapter#1
+// Question#1
+// alert("Welcome To My Website")
+
+// Question#2
+// alert("Error! Please enter a valid password")
+
+// Question#3
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+// Question#4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!")
+
+
+// Chapter#2
+
+// Question#1
+// var userName = ;
+
+// Question#2
+// var myName = "Ayan Sheikh";
+
+// Question#3
+// var message = "Hello World";
+// alert(message);
+
+// Question#4
+// var studentName = "Ayan Sheikh";
+// var studentAge = 17+" years old";
+// var certificate = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(certificate);
+
+// Question#5
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(pizza);
+
+// Question#6
+// var email = "ayansheikh@gmail.com"
+// alert("My email address is "+ email);
+
+// Question#7
+// var book = "A Smarter Way to learn JavaScript"
+// alert("I am trying to learn from book "+ book);
+
+// Question#8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Question#9
+// var xyz = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(xyz);
+
+
+// Chapter#3
+// Question#1
+// var age = 17;
+// alert("I am " + age + " years old");
+
+// Question#2
+// var visitTime = 14;
+// alert("You have visited this site " + visitTime + " times");
+
+// Question#3
+// var birthYear = 2006;
+// document.write("My birth year is " + birthYear+ "</br>");
+// document.write("Data type of my declared variable is number");
+
+// Question#4
+// var visitorName = prompt("Enter Your name");
+// var Qty = +prompt ("how many product you bought");
+// var product = "T-Shirt";
+// document.write(visitorName+ " " + "Ordered" +" "+ Qty +" "+ product + " on XYZ Clothing Store")
+
+
+// Chapter#4
+// Question#1
+// var name = "Ayan Sheikh", Age=17 , goal= "Successful Developer";
+
+// Question#2
+// legal Name
+// var userName, user_name , $userName ,userName1 ,user1name;
+// illegal Name
+// var user-name, user name , 1username , @username ;
+
+// Question#3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("Variable names can only contain <b><u>Numbers</u></b> , <b><u>letters</u></b>, <b><u>$</u></b> and <b><u>_</u>. For example $my_1stVariable</b><br> ");
+// document.write("Variables must begin with a <u><b>letter</b></u> , <b><u>$</u></b> or <b><u>_</b></u>. For example <b>$name, _name or name</b> <br>");
+// document.write("Variable names are case <u><b>sensitive.</u></b><br>");
+// document.write("Variable names should not be JS <u><b>keyword</b></u>.")
+
+
+// Chapter#5
+// Question#1
+var num1 = 3;
+var num2 = 5;
+// Add condition
+var add ="The sum of 3 + 5 is "+ (num1 + num2);
+document.write(add+"<br>");
+var sub = "The sum of 3 - 5 is "+ (num1 - num2);
+document.write(sub + "<br>");
