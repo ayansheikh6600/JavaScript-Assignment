d9fe90a9386528d93ab62f6dcc6b3fe162ff0e8c
ayansheikh6600 <ayansheikh6600@email.com>
2023-02-14 11:57:47 +0500

    chapter 5

--- app.js
+++ app.js
@@ -95,10 +95,92 @@
 
 // Chapter#5
 // Question#1
-var num1 = 3;
-var num2 = 5;
+// var num1 = 3;
+// var num2 = 5;
 // Add condition
-var add ="The sum of 3 + 5 is "+ (num1 + num2);
-document.write(add+"<br>");
-var sub = "The sum of 3 - 5 is "+ (num1 - num2);
-document.write(sub + "<br>");
+// var add ="The sum of 3 + 5 is "+ (num1 + num2);
+// document.write(add+"<br>");
+
+// Question#2
+// var sub = "The sum of 3 - 5 is "+ (num1 - num2);
+// document.write(sub + "<br>");
+// var sub = "The sum of 3 * 5 is "+ (num1 * num2);
+// document.write(sub + "<br>");
+// var sub = "The sum of 3 / 5 is "+ (num1 / num2);
+// document.write(sub + "<br>");
+// var sub = "The sum of 3 % 5 is "+ (num1 % num2);
+// document.write(sub + "<br>");
+
+// Question#3
+// var num1;
+// var intialvalue = 5;
+// document.write("Value after variable declaration is " + num1 + "<br>");
+// document.write("Initial value is " + intialvalue +  "<br>");
+// var increamentvalue = ++intialvalue;
+// document.write("Value after increament is " + intialvalue +"<br>");
+// var intialvalue = intialvalue+7;
+// document.write("Value after addition is " + intialvalue+"<br><br>");
+
+// var decrementvalue= --intialvalue;
+// document.write("Value after decrament is " + intialvalue+"<br>");
+// var reminder = intialvalue % 3;
+// document.write("The remainder is " + reminder);
+
+// Question#4
+// var ticket = 600;
+// var ticket_5 = ticket*5;
+// document.write("Total cost to buy 5 tickets to movie is "+ ticket_5 + "PKR");
+
+// Question#5
+// document.write("<b>Table of 4</b> <br>");
+// var table = 4;
+// var answer= 4*1;
+// var answer1= 4*2;
+// var answer2= 4*3;
+// var answer3= 4*4;
+// var answer4= 4*5;
+// var answer5= 4*6;
+// var answer6= 4*7;
+// var answer7= 4*8;
+// var answer8= 4*9;
+// var answer9= 4*10;
+// document.write(table + " x "+ 1 +"= "+ answer +"<br>");
+// document.write(table + " x "+ 2 +"= "+ answer1 +"<br>");
+// document.write(table + " x "+ 3 +"= "+ answer2 +"<br>");
+// document.write(table + " x "+ 4 +"= "+ answer3 +"<br>");
+// document.write(table + " x "+ 5 +"= "+ answer4 +"<br>");
+// document.write(table + " x "+ 6 +"= "+ answer5 +"<br>");
+// document.write(table + " x "+ 7 +"= "+ answer6 +"<br>");
+// document.write(table + " x "+ 8 +"= "+ answer7 +"<br>");
+// document.write(table + " x "+ 9 +"= "+ answer8 +"<br>");
+// document.write(table + " x "+ 10 +"= "+ answer9 +"<br>");
+
+// Question#6
+// var F = 77;
+// var C = (F -32) * 5/9;
+// document.write(C + "<sup>o</sup>C is 77<sup>o</sup>F \n");
+
+// var C = 21.11111111111111111
+// var F = (C *9 / 5) + 32;
+// document.write(F + "<sup>o</sup>F is 21.1111<sup>o</sup>C");
+
+// Question#7
+// var item1 = 650;
+// var item2 = 100;
+// var qtyItem1=3;
+// var qtyItem2 =7;
+// var shippingCharges = 100;
+// var totalCost= (650*3)+ (100*7)+100;
+// document.write("Total cost of your order is " + totalCost);
+
+// Quetion#8
+// var totalmarks= +prompt("Enter Total Marks...");
+// var obtainedMarks = +prompt("Enter Obtained Marks...")
+// var percentage= (obtainedMarks/totalmarks) * 100;
+// document.write("Your Percentage is "+ percentage);
+
+// Question#9
+// var dollar=10;
+// var saudiRiyal=25;
+// document.write("Total Currency in PKR: " + ((dollar*104.80)+(saudiRiyal*28)));
+
