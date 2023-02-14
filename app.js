d4534d2b0e116535811c3d73c9cc14cd6ba46e11
ayansheikh6600 <ayansheikh6600@email.com>
2023-02-15 01:28:51 +0500

    completed

--- app.js
+++ app.js
@@ -184,3 +184,242 @@
 // var saudiRiyal=25;
 // document.write("Total Currency in PKR: " + ((dollar*104.80)+(saudiRiyal*28)));
 
+// Question#10
+// var calculation = (((5+5))*10)/2;
+// console.log(calculation);
+
+//Question#11
+// var currentyear = +prompt("Enter current year");
+// var birthyear = +prompt("Enter your Birth Year");
+// var age = currentyear-birthyear;
+// document.write("Current Year: " + currentyear + "<br>");
+// document.write("Your Birth Year: " + birthyear + "<br>");
+// document.write("Your Age: "+age);
+
+// Question#13
+// var fav = "chocolate chip";
+// var age = 15;
+// var estimatedAge= 65;
+// var perDay= 3;
+// var total = ((estimatedAge-age)*perDay);
+// document.write("<h1> The Lifetime Supply Calculator</h1> <br>");
+// document.write("Favourite Snack: " + fav +"<br>");
+// document.write("Current Age: " + age +"<br>");
+// document.write("Estimated Maximium Age: " + estimatedAge +"<br>");
+// document.write("Ammount of snacks per day: " + perDay +"<br>");
+// document.write("You will need "+total + " chocolate chip to last you until the ripe old age of " + estimatedAge);
+
+// Chapter6to9
+// Question#1
+// var a = 10;
+// document.write("The vakue of a is: " + a +"<br>");
+// document.write("...................................."+"<br><br>");
+
+
+// document.write("The vakue of ++a is: " + ++a +"<br>");
+// document.write("Now the of a is: " + a +"<br><br>");
+
+
+// document.write("The vakue of a++ is: " + a++ +"<br>");
+// document.write("Now the of a is: " + a +"<br><br>");
+
+// document.write("The value of --a is: " + --a +"<br>");
+// document.write("Now the of a is: " + a +"<br><br>"); 
+
+// document.write("The value of a-- is: " + a-- +"<br>");
+// document.write("Now the of a is: " + a +"<br>");
+
+// Qustion#2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// //           --1 - --0 + ++1 + 1--;
+// document.write("a is: "+a + "<br>");
+// // value of a is 1
+// document.write("b is: "+b +"<br>");
+// // value of b is 0
+// document.write(result);
+// // the is 3
+
+// Question#3
+// var userName = prompt("Enter Your Name")
+// alert("Hi "+ userName + "\nWelcome To My Website");
+
+// Question#4&5
+// var table = +prompt("Enter A Table Number");
+// var answer1 = table*1;
+// var answer2 = table*2;
+// var answer3 = table*3;
+// var answer4 = table*4;
+// var answer5 = table*5;
+// var answer6 = table*6;
+// var answer7 = table*7;
+// var answer8 = table*8;
+// var answer9 = table*9;
+// var answer10 = table*10;
+// if(table){
+//     document.write(table+ "*"+ 1 + "=" + answer1 +"<br>"  );
+//     document.write(table+ "*"+ 2 + "=" + answer2 +"<br>"  );
+//     document.write(table+ "*"+ 3 + "=" + answer3 +"<br>"  );
+//     document.write(table+ "*"+ 4 + "=" + answer4 +"<br>"  );
+//     document.write(table+ "*"+ 5 + "=" + answer5 +"<br>"  );
+//     document.write(table+ "*"+ 6 + "=" + answer6 +"<br>"  );
+//     document.write(table+ "*"+ 7 + "=" + answer7 +"<br>"  );
+//     document.write(table+ "*"+ 8 + "=" + answer8 +"<br>"  );
+//     document.write(table+ "*"+ 9 + "=" + answer9 +"<br>"  );
+//     document.write(table+ "*"+ 10 + "=" + answer10 +"<br>"  );
+// }
+
+// Question#6
+// var totalMarkSubject1=100;
+// var subject1 = prompt("Enter Your Subject");
+// var mark1= +prompt("Enter your obtained Marks in "+subject1);
+
+
+
+// var totalMarkSubject2=100;
+// var subject2 = prompt("Enter Your Subject");
+// var mark2= +prompt("Enter your obtained Marks in "+subject2);
+
+// var totalMarkSubject3=100;
+// var subject3 = prompt("Enter Your Subject");
+// var mark3= +prompt("Enter your obtained Marks in "+subject3);
+
+// var subjectTotal= totalMarkSubject1 + totalMarkSubject2 + totalMarkSubject3;
+// var obtainedTotalMark = mark1 + mark2 + mark3;
+// var persubject1 = (mark1/totalMarkSubject1) * 100 +"%";
+// var persubject2 = (mark2/totalMarkSubject2) * 100 +"%";
+// var persubject3 = (mark3/totalMarkSubject3) * 100 +"%";
+
+// document.write("<b>Subject   Total Marks   Obtained Marks  Percentage</b> <br>")
+// document.write(subject1 +" "+ totalMarkSubject1+ " " + mark1 + " " +persubject1 + "<br>");
+// document.write(subject2 +" "+ totalMarkSubject2+ " " + mark2 + " " +persubject2 + "<br>");
+// document.write(subject3 +" "+ totalMarkSubject3+ " " + mark3 + " " +persubject3 + "<br>");
+
+// Chapter9to11
+// Question#1
+// var cityName = prompt("Enter Your Favourite City");
+// if (cityName=="Karachi"){
+//     alert("Welcome to city of light")
+// }else{
+
+// }
+
+// Question#2
+// var gender = prompt("Enter Your Gender");
+// if(gender=="male"){
+//     alert("Good Morning Sir")
+// }else if(gender=="female"){
+//     alert("Good Morning Ma'am")
+// }
+
+// Question#3
+// var signalColor = prompt("Enter Signal Color");
+// if(signalColor=="Red"){
+//     alert("Must Stop")
+// }else if(signalColor=="Yellow"){
+//     alert("Ready to Move")
+// }else if(signalColor=="Green"){
+//     alert("Move Now")
+// }
+
+// Question#4
+// var fuel = +prompt("Enter remaining Fuel");
+// if(fuel <0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+// Question#5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true"); //False
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");//False
+// }
+// if (c === 13){
+// alert("condition 2 is true");//True
+// }
+// if (++c < 14){
+// alert("condition 3 is true");//False
+// }
+// if(c === 14){
+// alert("condition 4 is true");//True
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals"); //True
+// }
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// Question#6
+// var total = +prompt("Enter Your Total Mark");
+// var obtained = +prompt("Enter Your Obtain Marks");
+// var percentage = (obtained/total) * 100;
+
+// document.write("<h1>Mark Sheet</h1><br>");
+// document.write("Total Marks: " + total+"<br>");
+// document.write("Obtained Marks: " + obtained+"<br>");
+// document.write("Percentage: " + percentage+"%<br>");
+
+// if(percentage>=80){
+//     document.write("Grade A-One <br>");
+//     document.write("Remarks: Excellent <br>");
+// }else if(percentage>=70){
+//     document.write("Grade A <br>");
+//     document.write("Remarks: Good <br>");
+// }else if(percentage>=60){
+//     document.write("Grade B <br>");
+//     document.write("Remarks: You need to improve <br>");
+// }else if(percentage<60){
+//     document.write("Fail <br>");
+//     document.write("Sorry <br>");
+// }
+
+// Question#7
+// var secretNum = 5;
+// var userNume = +prompt("Enter Any Num but ranging 1 to 10");
+// if(userNume==secretNum){
+//     alert("'Bingo!' Correct Answer")
+// }else if (userNume>5 && userNume<7){
+//     alert("So Close")
+// }
+
+
+// Chapter#12to13
+// Question#2
+// var num1 = +prompt("Enter a Number");
+// var num2 = +prompt("Enter a Number");
+// if(num1>num2){
+//     alert(num1)
+// }else if (num2>num1){
+//     alert(num2)
+// }
+
+// var num = +prompt("Enter a number");
+// if(num == (-num)){
+//     alert("Negative")
+// }else if(num== +num) {
+//     alert("Positive")
+// }else if (num == 0){
+//     alert("Zero")
+// }
